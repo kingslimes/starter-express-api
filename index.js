d13898609d99cfe2e00259b0f731e34405f50fbc
kingslime <72143717+kingslimes@users.noreply.github.com>
2023-04-19 01:16:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const { google } = require("googleapis");
+const express = require("express");
+const stream = require("stream");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const service = express();
+
+const DRIVE_FOLDER_ID = ['1-sQEbClcbj6xmywa5XygM3wWfGCWWF69'];
+const DRIVE_SCOPE = ['https://www.googleapis.com/auth/drive'];
+const DRIVE_KEY_PATH = path.join( __dirname, 'GoogleKey.json' );
+const MIDDLE = multer();
+
+service.use( cors() );
+
+service.all("/", ( req, res ) => {
+    res.json({
+        code: 200,
+        message: "Connection succeeded."
+    })
+});
+
+service.post("/api/v1/image", MIDDLE.any(), async ( req, res ) => {
+    
+    try {
+        
+        const bufferStream = GetBuffer( req.files[0] );
+        const auth = new google.auth.GoogleAuth({
+            keyFile: DRIVE_KEY_PATH,
+            scopes: DRIVE_SCOPE
+        })
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        })
+        const fileMetaData = {
+            'name': "UPLOAD_" + new Date().getTime(),
+            'parents': DRIVE_FOLDER_ID
+        }
+        const media = {
+            mimeType: 'image/avchd',
+            body: bufferStream
+        }
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            field: 'id'
+        })
+        await res.json({
+            id: response.data.id,
+            url: "https://drive.google.com/uc?id=" + response.data.id,
+            body: response.data
+        })
+        
+    } catch (err) { res.json({ error:true,message:err }) }
+    
+});
+
+function GetBuffer( file ) {
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end( file.buffer );
+    return bufferStream
+}
+
+service.listen( process.env.PORT || 8960 );
