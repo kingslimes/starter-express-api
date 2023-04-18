cf018f2f77bc4d47144584e788e085d38e3b25cb
kingslime <72143717+kingslimes@users.noreply.github.com>
2023-04-19 04:20:27 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const bodyParser = require("body-parser");
 const { google } = require("googleapis");
 const express = require("express");
 const stream = require("stream");
@@ -6,27 +5,19 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const service = express();
-
 const DRIVE_FOLDER_ID = ['1-sQEbClcbj6xmywa5XygM3wWfGCWWF69'];
 const DRIVE_SCOPE = ['https://www.googleapis.com/auth/drive'];
 const DRIVE_KEY_PATH = path.join( __dirname, 'GoogleKey.json' );
 const MIDDLE = multer();
-
 service.use( cors() );
-service.use( bodyParser.urlencoded({ extended:false,limit:"1024Mb" }) );
-service.use( bodyParser.json({ limit:"1024Mb" }) );
-
 service.all("/", ( req, res ) => {
     res.json({
         code: 200,
         message: "Connection succeeded."
     })
 });
-
 service.post("/api/v1/image", MIDDLE.single("image"), async ( req, res ) => {
-    
     try {
-        
         const bufferStream = GetBuffer( req.file );
         const auth = new google.auth.GoogleAuth({
             keyFile: DRIVE_KEY_PATH,
@@ -37,7 +28,7 @@ service.post("/api/v1/image", MIDDLE.single("image"), async ( req, res ) => {
             auth
         })
         const fileMetaData = {
-            'name': "UPLOAD_" + new Date().getTime(),
+            'name': "Image_" + new Date().getTime(),
             'parents': DRIVE_FOLDER_ID
         }
         const media = {
@@ -52,22 +43,13 @@ service.post("/api/v1/image", MIDDLE.single("image"), async ( req, res ) => {
         await res.json({
             id: response.data.id,
             url: "https://drive.google.com/uc?id=" + response.data.id,
-            body: response.data
+            name: response.data.name
         })
-        
     } catch (err) { res.json({ error:true,message:err }) }
-    
 });
-
 function GetBuffer( file ) {
     const bufferStream = new stream.PassThrough();
     bufferStream.end( file.buffer );
     return bufferStream
 }
-
-service.post("/api/test", async ( req, res ) => {
-    console.log( req.body );
-    await res.json({ result:req.body });
-})
-
 service.listen( process.env.PORT || 8960 );
